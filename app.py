675cdcbf2956d1791a33fdc2bd70b808ef766c87
UGALDE <angelugaldehernandez@gmail.com>
2025-09-22 08:29:07 -0600

    feat:suma

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ print(f"IP address: {IPAddress}")
 
 for i in range(10):
     print(f"count: {i}")
+
+
+numero_a = input("Dame el primer numero: ")
+numero_b = input("Dame el segundo numero: ")
+print(f"la sumas de los dos numeros es: {numero_a + numero_b}")
